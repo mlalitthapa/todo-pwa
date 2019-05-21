24aa85b13d80fd23fb524362c994ea9bb0eb75bb
Lalit Thapa <lalit.thapa@ekbana.info>
2019-05-21 17:39:18 +0545

    Remove actions for loaders

--- src/store/task.js
+++ src/store/task.js
@@ -1,5 +1,6 @@
 import mutations from '@/config/mutations'
 import actions from '@/config/actions'
+import loaders from '@/config/loaders'
 import { addTask, fetchTasks } from '@/services/task.service'
 import router from '@/router'
 
@@ -7,35 +8,31 @@ export default {
   namespaced: true,
   state: {
     tasks: [],
-    loading: false
+    loading: ''
   },
   mutations: {
     [mutations.ADD_TASK] (state, payload) {
       state.tasks[payload.key] = payload.task
     },
-    [mutations.TOGGLE_LOADER] (state, status) {
-      state.loading = status
+    [mutations.TOGGLE_LOADER] (state, loader) {
+      state.loading = loader
     },
     [mutations.FETCH_TASKS] (state, payload) {
       state.tasks = payload.tasks
     }
   },
   actions: {
-    [actions.ADD_TASK] ({ dispatch }, task) {
+    [actions.ADD_TASK] ({ dispatch, commit }, task) {
+      commit(mutations.TOGGLE_LOADER, loaders.ADDING_TASK)
       addTask(task)
     },
     [actions.TASK_ADDED] ({ commit }, payload) {
       commit(mutations.ADD_TASK, payload)
+      commit(mutations.TOGGLE_LOADER, '')
       router.push('/')
     },
-    [actions.START_LOADER] ({ commit }) {
-      commit(mutations.TOGGLE_LOADER, true)
-    },
-    [actions.STOP_LOADER] ({ commit }) {
-      commit(mutations.TOGGLE_LOADER, false)
-    },
-    [actions.FETCH_TASKS] ({ dispatch }) {
-      dispatch(actions.START_LOADER)
+    [actions.FETCH_TASKS] ({ dispatch, commit }) {
+      commit(mutations.TOGGLE_LOADER, loaders.FETCHING_TASKS)
       fetchTasks()
     }
   }
