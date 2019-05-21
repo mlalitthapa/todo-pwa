880f7fd898aaf31dc7686777f0c97f64a7397b02
Lalit Thapa <lalit.thapa@ekbana.info>
2019-05-21 12:02:18 +0545

    Add firebase service to communicate with firebase api

---
+++ src/services/firebase_service.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+import store from '@/store'
+import { TASK_ADDED } from '@/config/actions'
+
+const init = () => {
+  if (firebase.apps.length) return
+  const config = {
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: `${process.env.VUE_APP_FIREBASE_APP_NAME}.firebaseapp.com`,
+    databaseURL: `https://${process.env.VUE_APP_FIREBASE_APP_NAME}.firebaseio.com`,
+    storageBucket: `${process.env.VUE_APP_FIREBASE_APP_NAME}.appspot.com`
+  }
+  firebase.initializeApp(config)
+}
+
+const getDBReference = () => {
+  init()
+  return firebase.database()
+}
+
+export const pushToRef = (task) => {
+  const db = getDBReference()
+  db.ref('tasks').push(task)
+    .then(() => {
+      store.dispatch(TASK_ADDED, task)
+    })
+    .catch(console.log)
+}
