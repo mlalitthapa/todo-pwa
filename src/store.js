897d9a1eac3d1785131a732f71424d6aac8ace2d
Lalit Thapa <lalit.thapa@ekbana.info>
2019-05-21 12:03:44 +0545

    Change actions

--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 
 import actions from '@/config/actions'
 import mutations from '@/config/mutations'
+import { addTask } from '@/services/task_service'
 
 Vue.use(Vuex)
 
@@ -16,7 +17,10 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    [actions.ADD_TASK] ({ commit }, task) {
+    [actions.ADD_TASK] ({ dispatch }, task) {
+      addTask(task)
+    },
+    [actions.TASK_ADDED] ({ commit }, task) {
       commit(actions.ADD_TASK, { task })
     }
   }
